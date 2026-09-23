7ffcc97e14502bebef3250e5640a62b0fe0ded20
Ajay Mathew <ajayedathazhe@gmail.com>
2026-09-24 09:22:07 +1000

    feat(frontend): add component-loader and page-loader

---
+++ frontend/shared/components/feedback/loader/index.ts
@@ -0,0 +1,7 @@
+export { default as ComponentLoader } from "./component-loader";
+export { default as PageLoader } from "./page-loader";
+export type {
+  ComponentLoaderProps,
+  LoaderSize,
+  PageLoaderProps,
+} from "./loader.types";
--- frontend/shared/components/index.ts
+++ frontend/shared/components/index.ts
@@ -1,2 +1,3 @@
+export * from "./feedback";
 export * from "./layout";
 export * from "./ui";
